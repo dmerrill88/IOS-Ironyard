d3653675ab20ac1ac6be34984242888578b8f9b9
dmerrill88 <DanielMerrill@Daniels-MacBook-Pro.local>
2014-08-26 15:04:05 -0400

    Balloons

---
+++ Balloons/Balloons/BALAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BALAppDelegate.h
+//  Balloons
+//
+//  Created by Daniel Merrill on 8/21/14.
+//  Copyright (c) 2014 Daniel Merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BALAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Balloons/Balloons/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Balloons
+//
+//  Created by Daniel Merrill on 8/21/14.
+//  Copyright (c) 2014 Daniel Merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BALAppDelegate class]));
+    }
+}
