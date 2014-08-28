7b8f772144a4ea6711185c7ff0f05ffae1e4da74
dmerrill88 <DanielMerrill@Daniels-MBPro.local>
2014-08-28 14:03:58 -0400

    the list for obJC
    the

---
+++ The List/The List/TLAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TLAAppDelegate.h
+//  The List
+//
+//  Created by Daniel Merrill on 8/22/14.
+//  Copyright (c) 2014 Daniel Merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ The List/The List/TLAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TLAViewController.h
+//  The List
+//
+//  Created by Daniel Merrill on 8/22/14.
+//  Copyright (c) 2014 Daniel Merrill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLAViewController : UIViewController
+
+@end
