29f46013cfd42cd901fb7807ea98a7565367298c
dmerrill88 <DanielMerrill@Daniels-MBPro.local>
2014-08-28 14:02:11 -0400

    Parse for Swift

---
+++ SwiftParse/SwiftParse/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  SwiftParse
+//
+//  Created by Heidi Proske on 8/26/14.
+//  Copyright (c) 2014 Heidi Proske. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+    var window: UIWindow?
+
+
+    func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
